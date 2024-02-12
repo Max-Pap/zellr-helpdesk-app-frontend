4caf0b03a8067f5abd4bc4dfa73b7fca21935834
Max-Pap <maksym.pap@proton.me>
2024-02-12 16:27:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zellr Helpdesk App
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss'
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Zellr Helpdesk App</h1>
+    </div>
+  )
+};
